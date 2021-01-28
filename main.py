2aa3ab33c6cfeb18dc5196bc718a884257c87e5a
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2021-01-28 21:55:54 +0500

    cleaned code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from bs4 import BeautifulSoup
 from selenium.webdriver import Firefox
 from progress.spinner import Spinner
+from selenium.webdriver.firefox.options import Options
 
-from urllib.request import urlopen
 url = 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fkfueit.official%2F&tabs=timeline&width=12000&_rdc=2&_rdr'
-firefox = Firefox(executable_path='/home/talha/Programs/geckodriver')
-spinner = Spinner('Loading ')
-spinner.start()
+options = Options()
+options.headless = True
+firefox = Firefox(options=options, executable_path='/home/talha/Programs/geckodriver')
+print('Opening browser...')
 firefox.get(url)
 
 # wait until posts are being loaded
 posts = []
-
+spinner = Spinner('Loading ')
+spinner.start()
 while len(posts) == 0:
     spinner.next()
     posts = firefox.find_elements_by_tag_name("p")
